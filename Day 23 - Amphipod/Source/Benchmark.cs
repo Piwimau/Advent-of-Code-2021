f1f31b63a2b3d186effe6fbd8e45a5fab036d204
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2024-10-05 14:54:58 +0200

    Add benchmarks for all days.

---
+++ Day 23 - Amphipod/Source/Benchmark.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace Amphipod.Source;
+
+/// <summary>Represents a <see cref="Benchmark"/> for the <see cref="Amphipod"/> puzzle.</summary>
+[MemoryDiagnoser]
+public class Benchmark {
+
+    /// <summary>Runs a benchmark for the <see cref="Amphipod"/> puzzle.</summary>
+    [Benchmark]
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Benchmarking static methods is not supported."
+    )]
+    public void Run() => Amphipod.Solve(TextWriter.Null);
+
+}
---
+++ Day 25 - Sea Cucumber/Source/Benchmark.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace SeaCucumber.Source;
+
+/// <summary>
+/// Represents a <see cref="Benchmark"/> for the <see cref="SeaCucumber"/> puzzle.
+/// </summary>
+[MemoryDiagnoser]
+public class Benchmark {
+
+    /// <summary>Runs a benchmark for the <see cref="SeaCucumber"/> puzzle.</summary>
+    [Benchmark]
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Benchmarking static methods is not supported."
+    )]
+    public void Run() => SeaCucumber.Solve(TextWriter.Null);
+
+}
