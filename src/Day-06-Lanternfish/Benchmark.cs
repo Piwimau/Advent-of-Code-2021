ea36cea3617d12ffb97ecec0f2f7eb020d5fdf72
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2025-05-26 21:37:45 +0200

    Revise project structure.

---
+++ src/Day-06-Lanternfish/Benchmark.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace Lanternfish;
+
+/// <summary>
+/// Represents a <see cref="Benchmark"/> for the <see cref="Lanternfish"/> puzzle.
+/// </summary>
+[MemoryDiagnoser]
+public class Benchmark {
+
+    /// <summary>Runs a benchmark for the <see cref="Lanternfish"/> puzzle.</summary>
+    [Benchmark]
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Benchmarking static methods is not supported."
+    )]
+    public void Run() => Lanternfish.Solve(TextWriter.Null);
+
+}
---
+++ src/Day-17-Trick-Shot/Benchmark.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace TrickShot;
+
+/// <summary>Represents a <see cref="Benchmark"/> for the <see cref="TrickShot"/> puzzle.</summary>
+[MemoryDiagnoser]
+public class Benchmark {
+
+    /// <summary>Runs a benchmark for the <see cref="TrickShot"/> puzzle.</summary>
+    [Benchmark]
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Benchmarking static methods is not supported."
+    )]
+    public void Run() => TrickShot.Solve(TextWriter.Null);
+
+}
